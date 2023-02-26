470e2e30e0c93cdda9ed6ae04933ecc88f1439c5
azhryl <azhryl.akbar.2105356@students.um.ac.id>
2023-02-26 23:21:57 +0700

    delete test page and edit splash

--- lib/layers/presentation/pages/splash_screen.dart
+++ lib/layers/presentation/pages/splash_screen.dart
@@ -1,42 +1,28 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, library_private_types_in_public_api
 
 import 'dart:async';
-import 'package:ailment_alleviate/layers/presentation/pages/tes_onboard.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../../constants/custom_style.dart';
+import '../../../routes/router.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Splash Screen',
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(
-        Duration(seconds: 3),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => Onboarding())));
+    Timer(Duration(seconds: 3), () => router.pushNamed('emperis'));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(98, 70, 234, 1.000),
+      backgroundColor: primary,
       body: Center(
         child: Text('Alleviate',
             style: GoogleFonts.comfortaa(
