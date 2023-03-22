72d7bf076b05b7915a347e39243a70863869e2c2
r_g7d <danangr123@gmail.com>
2023-03-22 19:55:27 +0700

    ios navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cupertino_back_gesture/cupertino_back_gesture.dart';
 import 'package:flutter/material.dart';
 import 'package:ailment_alleviate/routes/router.dart';
 // import 'package:provider/provider.dart';
@@ -12,11 +13,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerDelegate: router.routerDelegate,
-      routeInformationParser: router.routeInformationParser,
-      routeInformationProvider: router.routeInformationProvider,
+    return BackGestureWidthTheme(
+      backGestureWidth: BackGestureWidth.fraction(1 / 2),
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerDelegate: router.routerDelegate,
+        routeInformationParser: router.routeInformationParser,
+        routeInformationProvider: router.routeInformationProvider,
+        theme: ThemeData(
+          pageTransitionsTheme: const PageTransitionsTheme(
+            builders: {
+              TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+              TargetPlatform.iOS:
+                  CupertinoPageTransitionsBuilderCustomBackGestureWidth(),
+            },
+          ),
+        ),
+      ),
     );
   }
 }
